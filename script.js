02e8ede3db82be5949c07d9c2ddfe9945783db93
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-07 22:10:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+ // Initialize the counter value
+    let counterValue = 0;
+
+    // Function to increment the counter and display an alert
+    function incrementCounter() {
+      // Display an alert with the un-incremented value
+      alert("Un-incremented value: " + counterValue);
+
+      // Increment the counter value
+      counterValue++;
+
+      // Update the counter value in the HTML
+      document.getElementById('counter').innerText = counterValue;
+    }
